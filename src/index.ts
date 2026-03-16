6e433ab9b3bbb4657cf9a479f4f81992ddd4e42b
Alex Sandro <alexsandro49.dev@gmail.com>
2026-03-16 19:00:15 -0300

    feat: vercel integration

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import cors from 'cors';
 import { productRoutes } from './routes/product';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", productRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-});
+export default app
+
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+    });
+}
