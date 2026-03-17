2d820af7fba5fdf571ea3678b074176180fab4a8
Alex Sandro <alexsandro49.dev@gmail.com>
2026-03-17 09:13:47 -0300

    fix: origin cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL || 'http://localhost:1573', 
     methods: ['GET'],
     credentials: true
 }));
